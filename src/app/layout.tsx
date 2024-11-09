f0b6ef655c0769beade5c6e351e06e62dfdc3c12
Pratul Makar <pratulmakar02@gmail.com>
2024-11-09 23:22:59 +0530

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main> {children}</main>
       </body>
     </html>
   );
