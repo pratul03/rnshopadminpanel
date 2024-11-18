a3dc84a6ff799a080067ccd1d12cef086ce74f2c
Pratul Makar <pratulmakar02@gmail.com>
2024-11-19 01:41:57 +0530

    nnnn

--- src/actions/categories.ts
+++ src/actions/categories.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { CategoriesWithProductsResponse } from "@/app/admin/categories/categories.types";
+import { CreateCategorySchemaServer } from "@/app/admin/categories/create-category.schema";
 import { createClient } from "@/supabase/server";
+import slugify from "slugify";
 
 export const getCategoriesWithProducts =
   async (): Promise<CategoriesWithProductsResponse> => {
@@ -15,3 +17,44 @@ export const getCategoriesWithProducts =
 
     return data || [];
   };
+export const imageUploadHandler = async (formData: FormData) => {
+  const supabase = createClient();
+  if (!formData) return;
+  const fileEntry = formData.get("file");
+  if (!(fileEntry instanceof File)) throw new Error("Expected a file");
+  const fileName = fileEntry.name;
+
+  try {
+    const { data, error } = await supabase.storage
+      .from("app-images")
+      .upload(fileName, fileEntry, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+    if (error) {
+      console.log("Error while uploading image", error);
+      throw new Error("Error uploading image");
+    }
+    const {
+      data: { publicUrl },
+    } = await supabase.storage.from("app-images").getPublicUrl(data.path);
+    return publicUrl;
+  } catch (error) {
+    console.log("Error while uploading image", error);
+    throw new Error("Error uploading image");
+  }
+};
+
+export const createCategories = async ({
+  imageUrl,
+  name,
+}: CreateCategorySchemaServer) => {
+  const supabase = createClient();
+  const slug = slugify(name, { lower: true });
+
+  const { data, error } = await supabase.from("category").insert({
+    name,
+    imageUrl,
+    slug,
+  });
+};
--- src/app/admin/categories/create-category.schema.ts
+++ src/app/admin/categories/create-category.schema.ts
@@ -8,3 +8,14 @@ export const createCategorySchema = z.object({
 });
 
 export type CreateCategorySchema = z.infer<typeof createCategorySchema>;
+
+export const createCategorySchemaServer = z.object({
+  imageUrl: z.string().min(2, "Image is required"),
+  name: z
+    .string()
+    .min(2, { message: "name must be at least 2 characters long" }),
+});
+
+export type CreateCategorySchemaServer = z.infer<
+  typeof createCategorySchemaServer
+>;
