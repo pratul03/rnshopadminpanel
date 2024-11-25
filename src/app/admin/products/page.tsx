ab311e3d7bc63d49f3544fbfcc432a4a49c4e6cc
Pratul Makar <pratulmakar02@gmail.com>
2024-11-26 00:53:05 +0530

    nn

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,7 @@
 import { getCategoriesWithProducts } from "@/actions/categories";
 import { ProductPageComponent } from "./page-component";
 
-export default async function Product() {
+export default async function Products() {
   const categories = await getCategoriesWithProducts();
 
   return <ProductPageComponent categories={categories} />;
